f2c598e8fc4ceea6e74555af7d793684bb656b5d
chan614 <pchan5076@gmail.com>
2022-05-05 13:05:47 +0900

    Add BooksDTO

---
+++ bookFinderApp/Structures/BooksDTO+.swift
@@ -0,0 +1,38 @@
+//
+//  BooksDTO.swift
+//  bookFinderApp
+//
+//  Created by 박지찬 on 2022/05/04.
+//
+
+import Foundation
+
+struct BooksDTO: Codable {
+    let kind: String
+    let totalItems: Int
+    let items: [Item]
+}
+
+struct Item: Codable {
+    let id: String
+    let kind: String
+    let etag: String
+    let volumeInfo: VolumeInfo
+}
+
+struct VolumeInfo: Codable {
+    let title: String
+    let authors: [String]
+    let publishedDate: String
+    let maturityRating: String
+    let allowAnonLogging: Bool
+    let contentVersion: String
+    let imageLinks: ImageLinks
+    let previewLink: String
+    let infoLink: String
+    let canonicalVolumeLink: String
+}
+
+struct ImageLinks: Codable {
+    let smallThumbnail, thumbnail: String
+}
